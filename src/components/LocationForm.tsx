4012e23700cc6cbd917121820d0b9af4cc4c7f15
Austin Booth <austinbooth@yahoo.co.uk>
2020-10-26 11:26:41 +0000

    adds autocomplete

--- src/components/LocationForm.tsx
+++ src/components/LocationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import * as api from "../api";
 
 interface LocationFormProps {
@@ -6,26 +6,110 @@ interface LocationFormProps {
 }
 
 const LocationForm: React.FC<LocationFormProps> = (props) => {
-  const locationInputRef = useRef<HTMLInputElement>(null);
+  // const locationInputRef = useRef<HTMLInputElement>(null);
+  const [inputLocation, setInputLocation] = useState("");
+  const [suggestions, setSuggestions] = useState<Array<string> | null>(null);
+  const [activeSuggestion, setActiveSuggestion] = useState<number>(0);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    const location = locationInputRef.current!.value;
+    clearFormAndChangeMapLocation(inputLocation);
+  };
+
+  const clearFormAndChangeMapLocation = (location: string) => {
+    setInputLocation("");
+    // const location = locationInputRef.current!.value;
     api
       .getLatLngFromName(location)
       .then(({ lat, lng }) => props.setCoords(lat, lng));
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const entered = event.target.value;
+
+    const locations = ["prague", "Berlin", "paris", "st petersburg", "Split"];
+    // const inputText = locationInputRef.current!.value;
+    const possibleOptions = locations.filter((location) =>
+      location.toLowerCase().includes(entered.toLowerCase())
+    );
+    possibleOptions.unshift(entered); // add whatever the user has typed as the first option
+    if (entered.length === 0) setSuggestions(null);
+    else setSuggestions(possibleOptions);
+    setInputLocation(entered);
+  };
+
+  const onSuggestionClick = (event: React.MouseEvent<HTMLElement>) => {
+    const selectedLocation = event.currentTarget.innerHTML;
+
+    setInputLocation(selectedLocation);
+    clearFormAndChangeMapLocation(selectedLocation);
+    setSuggestions(null);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (suggestions === null) return;
+    const { key: keyPressed } = event;
+
+    if (keyPressed === "ArrowUp") {
+      // decrement
+      setActiveSuggestion((currentActiveSuggestion) => {
+        return currentActiveSuggestion > 0 ? currentActiveSuggestion - 1 : 0;
+      });
+      setInputLocation(suggestions![activeSuggestion - 1]);
+    }
+    if (keyPressed === "ArrowDown") {
+      // increment
+      setActiveSuggestion((currentActiveSuggestion) =>
+        currentActiveSuggestion < suggestions!.length - 1
+          ? currentActiveSuggestion + 1
+          : suggestions!.length - 1
+      );
+      setInputLocation(suggestions![activeSuggestion + 1]);
+    }
+    if (keyPressed === "Enter") {
+      if (suggestions.length > 0)
+        setInputLocation(suggestions![activeSuggestion]);
+      setActiveSuggestion(0);
+      setSuggestions(null);
+    }
+  };
+
+  const onMouseHover = (event: React.MouseEvent<HTMLElement>) => {
+    const optionHoveredOver = event.currentTarget.innerHTML;
+    const indexOfOptionHoveredOver = suggestions!.indexOf(optionHoveredOver);
+    setActiveSuggestion(indexOfOptionHoveredOver);
+    setInputLocation(optionHoveredOver);
+  };
+
   return (
-    <div>
+    <div className="form-and-suggestions-container">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="location"
           name="location"
-          ref={locationInputRef}
+          // ref={locationInputRef}
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+          value={inputLocation}
         ></input>
         <button type="submit">Find</button>
+        {suggestions && (
+          <ul className="suggestions">
+            {suggestions.map((option, index) => (
+              <li
+                key={option}
+                onClick={onSuggestionClick}
+                onMouseEnter={onMouseHover}
+                className={
+                  index === activeSuggestion ? "active-suggestion" : undefined
+                }
+              >
+                {option}
+              </li>
+            ))}
+          </ul>
+        )}
       </form>
     </div>
   );
