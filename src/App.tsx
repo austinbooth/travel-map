ef3fb46b96a3aa9a7b779565723f35a6b6ff7a73
Austin Booth <austinbooth78@gmail.com>
2021-12-05 23:48:30 +0000

    adds mapbox css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import 'mapbox-gl/dist/mapbox-gl.css'
 import "./App.css"
 import LocationForm from "./components/LocationForm"
 import Map from "./components/Map"
