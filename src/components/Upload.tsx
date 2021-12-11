6a3390307b6ec1f902c9e8548feefb1db37f3ee3
Austin Booth <austinbooth78@gmail.com>
2021-12-11 15:26:49 +0000

    moves helper functions out to util file

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -5,6 +5,7 @@ import { storage } from '../firebaseSingleton'
 import { v4 as uuid } from 'uuid'
 import { DateTime } from 'luxon'
 import * as exifr from 'exifr'
+import { getDateFromFilename, earliestDateTime } from '../util'
 
 const Upload: FC = () => {
   const [selectedFile, setSelectedFile] = useState<File>()
@@ -17,23 +18,7 @@ const Upload: FC = () => {
       setSelectedFile(file)
     }
   }
-  const getDateFromFilename = (filename: string): DateTime => {
-    const filenameWithOutExt = filename.slice(0, -4) // need a regex that can take care of multiple file formats
-    const filenameDT = DateTime.fromFormat(filenameWithOutExt, 'yyyy-MM-dd HH.mm.ss').toUTC()    
-    return filenameDT
-  }
-  const earliestDateTime = (date1: DateTime, date2: DateTime): DateTime => {
-    if (date1.invalidReason && date2.invalidReason) {
-      throw new Error('Both datetime objects are invalid.')
-    }
-    if (date1.invalidReason) {
-      return date2
-    }
-    if (date2.invalidReason) {
-      return date1
-    }
-    return date1 <= date2 ? date1 : date2
-  }
+  
   const uploadFile = async () => {
     if (selectedFile) {
       try {
