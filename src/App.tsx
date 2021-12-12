1e601f078e3339892313d0977fe897c40c715d38
Austin Booth <austinb@thestartupfactory.tech>
2021-12-12 19:38:34 +0000

    only allows navigation to upload if logged in

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import {
   Routes,
   Route,
+  Navigate
 } from "react-router-dom"
 import 'mapbox-gl/dist/mapbox-gl.css'
 import "./App.css"
@@ -12,27 +13,32 @@ import SignIn from './components/auth/SignIn'
 import Upload from "./components/Upload"
 import { User } from './types'
 
-
-
 const App = () => {
-  // eslint-disable-next-line
+  const [loading, setLoading] = useState(true)
   const [user, setUser] = useState<User | undefined>()
 
   useEffect(() => {
+    console.log('Running auth useEffect again...')
     auth.onAuthStateChanged((newFirebaseUser) => {
+      setLoading(true)
       if (newFirebaseUser?.displayName && newFirebaseUser.uid) {
         setUser({name: newFirebaseUser?.displayName, uid: newFirebaseUser?.uid})
       }
+      setLoading(false)
       console.log('Logged in', newFirebaseUser?.displayName)
     })
   }, [])
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
 
   return (
     <Routes>
       <Route path='/'      element={<Home />}   />
       <Route path='signin' element={<SignIn />} />
       <Route path='signup' element={<SignUp />} />
-      <Route path='upload' element={<Upload />} />
+      <Route path='upload' element={user ? <Upload /> : <Navigate to='/signin' replace />} />
     </Routes>
   )
 }
