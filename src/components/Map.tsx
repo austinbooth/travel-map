4a104ac35baac25417bd6d3038ee3ba745fdb71b
Austin Booth <austinb@thestartupfactory.tech>
2021-12-12 22:59:20 +0000

    creates pins from data in firestore, WIP

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,9 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import MapGL, { Marker, Popup } from "react-map-gl"
 import redPin from "../images/red-pin.png"
+import { getAllMediaForUser } from "../firestoreUtils"
+import { auth } from '../firebaseSingleton'
+import { MediaData } from "../types"
 
 // Following 6 lines from https://stackoverflow.com/questions/65434964/mapbox-blank-map-react-map-gl-reactjs:
 import mapboxgl from 'mapbox-gl'
@@ -9,33 +12,6 @@ import mapboxgl from 'mapbox-gl'
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default 
-
-const visited = [
-  {
-    name: "Prague",
-    latitude: 50.0755,
-    longitude: 14.4378,
-    images: [
-      {
-        src:
-          "https://firebasestorage.googleapis.com/v0/b/travel-map-6fc3a.appspot.com/o/color_wheel_4_background.svg?alt=media&token=5e3ac8c6-fa5f-424c-a7d7-4de0c387b3ef",
-        alt: "Image alt text",
-      },
-    ],
-  },
-  {
-    name: "Berlin",
-    latitude: 52.52,
-    longitude: 13.405,
-    images: [
-      {
-        src:
-          "https://firebasestorage.googleapis.com/v0/b/travel-map-6fc3a.appspot.com/o/color_wheel_4_background.svg?alt=media&token=5e3ac8c6-fa5f-424c-a7d7-4de0c387b3ef",
-        alt: "Image alt text",
-      },
-    ],
-  },
-]
 
 interface Viewport {
   width: number
@@ -55,30 +31,43 @@ interface MapProps {
   }
   setViewport: (viewport: Viewport) => void
   popupInfo: string | null
-  setPopupInfo: (location: string | null) => void
+  setPopupInfo: (uid: string | null) => void
 }
 
 const Map: FC<MapProps> = ({viewport, setViewport, popupInfo, setPopupInfo}) => {
-  const pinData = visited.map((pin) => (
+  const [data, setData] = useState<MediaData[]>()
+  useEffect(() => {
+    void (async () => {
+      if (auth.currentUser?.uid) {
+        const data = await getAllMediaForUser(auth.currentUser?.uid)
+        console.log('DATA:', data)
+        setData(data as MediaData[])
+      }
+    })()
+  }, [])
+  if (!data) {
+    return <p>Loading...</p>
+  }
+  const pinData = data.map((pin) => (
     <Marker
-      key={pin.name}
+      key={pin.uid}
       longitude={pin.longitude}
       latitude={pin.latitude}
       offsetTop={-25}
       offsetLeft={-15}
     >
-      <div className="pin" onClick={() => setPopupInfo(pin.name)}>
+      <div className="pin" onClick={() => setPopupInfo(pin.uid)}>
         <img src={redPin} alt={"pin"} />
       </div>
     </Marker>
   ))
 
-  const popUp = (find: string) => {
-    const info = visited.find((location) => location.name === find)
+  const popUp = (uid: string) => {
+    const info = data.find((location) => location.uid === uid)
     if (!info) {
       return <div>Location not found</div>
     }
-    console.log(info.name)
+    console.log(info.city)
     return (
       <Popup
         tipSize={5}
@@ -89,8 +78,8 @@ const Map: FC<MapProps> = ({viewport, setViewport, popupInfo, setPopupInfo}) => 
         onClose={() => setPopupInfo(null)}
       >
         <div onClick={()=>console.log('clicked!')}>
-          {info.name}
-          <img src={info.images[0].src} alt={info.images[0].alt}></img>
+          {info.city}
+          {/* <img src={info.images[0].src} alt={info.images[0].alt}></img> */}
         </div>
       </Popup>
     )
