5331981d50c8f878f3abb51739294cb748e35195
Austin Booth <austinb@thestartupfactory.tech>
2021-12-12 22:53:26 +0000

    adds a media type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,20 @@
+import { Timestamp } from 'firebase/firestore'
+
 type Uid = string // maybe refine in future
 
 export interface User {
   name: string
   uid: Uid
+}
+
+export interface MediaData {
+  uid: string
+  imageUri: string
+  thumbnailUri : string
+  rotation: number
+  datetime: Timestamp
+  latitude: number
+  longitude: number
+  city: string
+  country: string
 }
