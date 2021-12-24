873f7f36083c1bea9bde9d1109689d8ecb673415
Austin Booth <austinb@thestartupfactory.tech>
2021-12-24 13:05:08 +0000

    adds types for processed media data

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,3 +18,18 @@ export interface MediaData {
   place: string
   country: string
 }
+
+export interface ImageData {
+  uid: string
+  imageUri: string
+  thumbnailUri : string
+  rotation: number
+  datetime: Timestamp
+}
+export interface MediaDataProcessed {
+  latitude: number
+  longitude: number
+  place: string
+  country: string
+  images: ImageData[]
+}
