03cfdce9d53a2b4b01d0a52652deca4f1645987d
Austin Booth <austinbooth78@gmail.com>
2021-12-24 15:38:26 +0000

    when there is more than one image, checks wheteher the dates are the same, and if so only returns one date

--- src/tests/getDateOrDateRange.test.ts
+++ src/tests/getDateOrDateRange.test.ts
@@ -1,7 +1,8 @@
 import { getDateOrDateRange } from '../util'
 import {
   processedData, s1Timestamp, g1Timestamp, g2Timestamp,
-  n2Timestamp_latest, n3Timestamp_earliest
+  n2Timestamp_latest, n3Timestamp_earliest,
+  l1Timestamp
 } from './testData'
 import { DateTime } from 'luxon'
 
@@ -21,5 +22,10 @@ describe('getDateOrDateRange fn', () => {
     const n2Timestamp_latest_string = DateTime.fromJSDate(n2Timestamp_latest.toDate()).toFormat('dd LLL yyyy')
     const n3Timestamp_earliest_string = DateTime.fromJSDate(n3Timestamp_earliest.toDate()).toFormat('dd LLL yyyy')
     expect(result).toBe(`${n3Timestamp_earliest_string} - ${n2Timestamp_latest_string}`)
+  })
+  test('Only return one date when all the images have the same date', () => {
+    const result = getDateOrDateRange(processedData[3])
+    const l1TimestampString = DateTime.fromJSDate(l1Timestamp.toDate()).toFormat('dd LLL yyyy')
+    expect(result).toEqual(l1TimestampString)
   })
 })
