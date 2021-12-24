b18e26942907976f76bf170ec0975fdb6524c096
Austin Booth <austinb@thestartupfactory.tech>
2021-12-24 21:14:18 +0000

    adds fns to add a new user and to get all users

--- src/firestoreUtils.ts
+++ src/firestoreUtils.ts
@@ -1,6 +1,17 @@
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, collection, doc, setDoc } from 'firebase/firestore'
 import { getDownloadURL, ref } from 'firebase/storage'
 import { db, storage } from './firebaseSingleton'
+
+export const addNewUserToFirestore = async (uid: string) => {
+  const userRef = doc(db, '/users/', uid)
+  return await setDoc(userRef, {uid})
+}
+
+export const getAllUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, `/users/`))
+  const { docs } = querySnapshot
+  return docs.map(doc => doc.data()).map(doc => doc.uid) as string[]
+}
 
 export const getAllMediaForUser = async (uid: string) => {
   const querySnapshot = await getDocs(collection(db, `users/${uid}/media`))
