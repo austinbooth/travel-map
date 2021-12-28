82ab8d62e05dc8521a72ec7dbafaba6d8486de27
Austin Booth <austinbooth78@gmail.com>
2021-12-28 16:38:55 +0000

    refactors getPlaceFromLatLng and adds more data returned from endpoint

--- src/api.ts
+++ src/api.ts
@@ -22,13 +22,14 @@ export const getLatLngFromName = async (
 
 export const getPlaceFromLatLng = async ({lat, lng}: Coords) => {
   const api_url = "https://api.opencagedata.com/geocode/v1/json"
-  const request_url = `${api_url}?key=${process.env.REACT_APP_OPENCAGE_KEY}&q=${lat}+${lng}`
+  const request_url = `${api_url}?key=${process.env.REACT_APP_OPENCAGE_KEY}&q=${lat}+${lng}&no_annotations=1&limit=1`
   try {
-    const data = (await axios.get(request_url)).data.results[0].components
-    console.log('-->', data)
+    const [apiData] = (await axios.get(request_url)).data.results
     return {
-      place: data.city ?? data.village ?? data.hamlet,
-      country: data.country
+      geo_data: apiData,
+      place_full: apiData.formatted,
+      place: (apiData.formatted as string).split(',')[0],
+      country: apiData.components.country
     }
   } catch (err) {
     console.error(err)
