d8354bda35075dccb18f6b5621ae0201aa327a25
Austin Booth <austinb@thestartupfactory.tech>
2021-12-28 17:01:55 +0000

    deletes the now redundant groupMedia fn along with the test file for this fn and test data

--- src/tests/testData.ts
+++ src/tests/testData.ts
@@ -12,105 +12,6 @@ export const n3Timestamp_earliest = Timestamp.fromDate(DateTime.now().minus({day
 
 export const l1Timestamp = Timestamp.fromDate(DateTime.now().minus({days: 2}).toJSDate())
 
-export const testData: MediaData[] = [
-  {
-    uid: 'S1',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: s1Timestamp,
-    imageUri: "gs://sheffield-1",
-    latitude: 53.427647,
-    longitude: -1.70495,
-    place: "Sheffield",
-    rotation: 90,
-    thumbnailUri: "gs://sheffield-1-thumb"
-  },
-  {
-    uid: 'G1',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: g1Timestamp,
-    imageUri: "gs://golspie-1",
-    latitude: 57.981414,
-    longitude: -3.942469,
-    place: "Golspie",
-    rotation: 90,
-    thumbnailUri: "gs://golspie-1-thumb"
-  },
-  {
-    uid: 'G2',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: g2Timestamp,
-    imageUri: "gs://golspie-2",
-    latitude: 57.98196897,
-    longitude: -3.94452397,
-    place: "Golspie",
-    rotation: 90,
-    thumbnailUri: "gs://golspie-2-thumb"
-  },
-  {
-    uid: 'N1',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: n1Timestamp,
-    imageUri: "gs://nottingham-1",
-    latitude: 52.9548,
-    longitude: -1.1581,
-    place: "Nottingham",
-    rotation: 90,
-    thumbnailUri: "gs://nottingham-1-thumb"
-  },
-  {
-    uid: 'N2',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: n2Timestamp_latest,
-    imageUri: "gs://nottingham-2",
-    latitude: 52.9548,
-    longitude: -1.1581,
-    place: "Nottingham",
-    rotation: 90,
-    thumbnailUri: "gs://nottingham-2-thumb"
-  },
-  {
-    uid: 'N3',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: n3Timestamp_earliest,
-    imageUri: "gs://nottingham-3",
-    latitude: 52.9548,
-    longitude: -1.1581,
-    place: "Nottingham",
-    rotation: 90,
-    thumbnailUri: "gs://nottingham-3-thumb"
-  },
-  {
-    uid: 'L1',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: l1Timestamp,
-    imageUri: "gs://london-1",
-    latitude: 51.5072,
-    longitude: -0.1276,
-    place: "London",
-    rotation: 90,
-    thumbnailUri: "gs://london-1-thumb"
-  },
-  {
-    uid: 'L2',
-    user: 'U1',
-    country: "United Kingdom",
-    datetime: l1Timestamp,
-    imageUri: "gs://london-2",
-    latitude: 51.5072,
-    longitude: -0.1276,
-    place: "London",
-    rotation: 90,
-    thumbnailUri: "gs://london-2-thumb"
-  },
-]
-
 export const processedData: MediaDataProcessed[] = [
   {
     uid: 'uid0',
@@ -121,7 +22,6 @@ export const processedData: MediaDataProcessed[] = [
     user: 'U1',
     images: [
       {
-        // uid: 'S1',
         imageUri: "gs://sheffield-1",
         thumbnailUri: "gs://sheffield-1-thumb",
         rotation: 90,
@@ -141,7 +41,6 @@ export const processedData: MediaDataProcessed[] = [
     user: 'U1',
     images: [
       {
-        // uid: 'G1',
         imageUri: "gs://golspie-1",
         thumbnailUri: "gs://golspie-1-thumb",    
         rotation: 90,
@@ -151,7 +50,6 @@ export const processedData: MediaDataProcessed[] = [
         geo_data: {}
       },
       {
-        // uid: 'G2',
         imageUri: "gs://golspie-2",
         thumbnailUri: "gs://golspie-2-thumb",
         rotation: 90,
@@ -171,7 +69,6 @@ export const processedData: MediaDataProcessed[] = [
     user: 'U1',
     images: [
       {
-        // uid: 'N1',
         imageUri: "gs://nottingham-1",
         thumbnailUri: "gs://nottingham-1-thumb",    
         rotation: 90,
@@ -181,7 +78,6 @@ export const processedData: MediaDataProcessed[] = [
         geo_data: {}
       },
       {
-        // uid: 'N2',
         imageUri: "gs://nottingham-2",
         thumbnailUri: "gs://nottingham-2-thumb",    
         rotation: 90,
@@ -191,7 +87,6 @@ export const processedData: MediaDataProcessed[] = [
         geo_data: {}
       },
       {
-        // uid: 'N3',
         imageUri: "gs://nottingham-3",
         thumbnailUri: "gs://nottingham-3-thumb",    
         rotation: 90,
@@ -211,7 +106,6 @@ export const processedData: MediaDataProcessed[] = [
     user: 'U1',
     images: [
       {
-        // uid: 'L1',
         imageUri: "gs://london-1",
         thumbnailUri: "gs://london-1-thumb",    
         rotation: 90,
@@ -221,7 +115,6 @@ export const processedData: MediaDataProcessed[] = [
         geo_data: {}
       },
       {
-        // uid: 'L2',
         imageUri: "gs://london-2",
         thumbnailUri: "gs://london-2-thumb",    
         rotation: 90,
