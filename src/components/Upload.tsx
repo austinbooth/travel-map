94b03d87646e1c4614887fbbb6576e7b1b91d1f7
Austin Booth <austinbooth78@gmail.com>
2021-12-29 19:58:15 +0000

    refactors the long upload function into several smaller ones

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,16 +1,9 @@
 import { FC, useState, ChangeEvent, useEffect } from 'react'
-import { ref, uploadBytes } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
-import { storage, db } from '../firebaseSingleton'
-import { v4 as uuid } from 'uuid'
-import { DateTime } from 'luxon'
 import * as exifr from 'exifr'
-import { getDateFromFilename, earliestDateTime } from '../util'
 import { getPlaceFromLatLng } from '../api'
-import { Timestamp as firestoreTimestamp, doc, setDoc, collection } from 'firebase/firestore'
-import { findMediaForLocationDataForUser } from '../firestoreUtils'
-import { mean } from 'lodash'
-import { ImageData, MediaData } from '../types'
+import { Timestamp as firestoreTimestamp } from 'firebase/firestore'
+import { getDateTimeForFile, getThumbnailAndRotationForFile, setImageAndThumbnailInFirestorage, setOrUpdateImageForLocationInFirestore } from '../firestoreUtils'
 
 const Upload: FC = () => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>()
@@ -36,72 +29,34 @@ const Upload: FC = () => {
   const uploadFile = () => {
     if (selectedFiles) {
       try {
+        const user = getAuth().currentUser
+        if (!user) {
+          throw new Error('User not logged in')
+        }
         setUploading(true)
         selectedFiles.forEach(async (selectedFile) => {
-          const user = getAuth().currentUser
-          if (!user) {
-            throw new Error('User not logged in')
-          }
-          
-          const datetimeFromFilename = getDateFromFilename(selectedFile.name)
-          const datetimeFromFile = DateTime.fromMillis(selectedFile.lastModified).toUTC()
-          const datetime = earliestDateTime(datetimeFromFilename, datetimeFromFile)
-
-          const dateFolder = `${datetime.day}-${datetime.month}-${datetime.year}`
-          const uploadFileName = uuid()
-          const firestoreFileRef = ref(storage, `${user.uid}/${dateFolder}/${uploadFileName}`)
-          await uploadBytes(firestoreFileRef, selectedFile)
-          const imageUri = firestoreFileRef.toString()
-          console.log('Uploaded:', imageUri)
-
+          const datetime = getDateTimeForFile(selectedFile)
           const { latitude, longitude } = await exifr.gps(selectedFile)
           const { geo_data, place_full, place, country } = await getPlaceFromLatLng({lat: latitude, lng: longitude})
-          const thumbnail = await exifr.thumbnail(selectedFile)
-          const r = await exifr.rotation(selectedFile)
+          const { thumbnail, rotation} = await getThumbnailAndRotationForFile(selectedFile)
 
-          if (!thumbnail || !r) {
-            throw new Error('Could not generate thumbnail or get rotation info')
-          }
-          const { deg } = r
-          const firestoreFileRefThumbnail = ref(storage, `${user.uid}/${dateFolder}/${uploadFileName}-thumb`)
-          await uploadBytes(firestoreFileRefThumbnail, thumbnail, {contentType: 'image/jpeg'})
-          const thumbnailUri = firestoreFileRefThumbnail.toString()
-          console.log('Uploaded thumbnail:', thumbnailUri)
+          const {
+            imageUri,
+            thumbnailUri
+          } = await setImageAndThumbnailInFirestorage(user, datetime, selectedFile, thumbnail)
 
-          const existingDbEntry = await findMediaForLocationDataForUser(user.uid, place_full) as MediaData[]
-          console.log('User:', user.uid)
-          console.log('--> db entry:', existingDbEntry)
-          if (existingDbEntry.length > 1) {
-            throw new Error('More than 1 DB entry found.')
-          }
-          const docRef = existingDbEntry.length === 0 ?
-            doc(collection(db, `users/${user.uid}`, 'media'))
-              : doc(db, `users/${user.uid}/media`, existingDbEntry[0].uid)
-          
-          const images: ImageData[] = [
-            ...existingDbEntry[0]?.images ?? [],
-            {
+          await setOrUpdateImageForLocationInFirestore({
+            userUid: user.uid, country, place, place_full,
+            imageData: {
+              datetime: firestoreTimestamp.fromDate(datetime.toJSDate()),
+              geo_data,
               imageUri,
               thumbnailUri,
-              rotation: deg,
-              datetime: firestoreTimestamp.fromDate(datetime.toJSDate()),
+              rotation,
               latitude,
-              longitude,
-              geo_data,
+              longitude
             }
-          ]
-    
-          const dbEntity = {
-            user: user.uid,
-            uid: docRef.id,
-            latitude: mean(images.map(image => image.latitude)),
-            longitude: mean(images.map(image => image.longitude)),
-            place,
-            place_full,
-            country,
-            images,
-          }        
-          await setDoc(docRef, dbEntity)
+          })
           incrementNumberUploaded()
         })
       } catch (err) {
--- src/firestoreUtils.ts
+++ src/firestoreUtils.ts
@@ -1,6 +1,13 @@
 import { getDocs, collection, doc, setDoc, query, where } from 'firebase/firestore'
-import { getDownloadURL, ref } from 'firebase/storage'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { db, storage } from './firebaseSingleton'
+import { DateTime } from 'luxon'
+import { v4 as uuid } from 'uuid'
+import { mean } from 'lodash'
+import * as exifr from 'exifr'
+import { getDateFromFilename, earliestDateTime } from './util'
+import { ImageData, MediaData } from './types'
+import { User } from 'firebase/auth'
 
 export const addNewUserToFirestore = async (uid: string) => {
   const userRef = doc(db, '/users/', uid)
@@ -33,4 +40,83 @@ export const getDownloadUrlFromUri = async (uri: string) => {
   } catch (err) {
     console.error(err)
   }
+}
+
+export const setImageAndThumbnailInFirestorage = async (
+  user: User,
+  datetime: DateTime,
+  selectedFile: File,
+  thumbnail: Buffer | Uint8Array
+) => {
+  const dateFolder = `${datetime.day}-${datetime.month}-${datetime.year}`
+  const uploadFileName = uuid()
+  const firestoreFileRef = ref(storage, `${user.uid}/${dateFolder}/${uploadFileName}`)
+  await uploadBytes(firestoreFileRef, selectedFile)
+  const imageUri = firestoreFileRef.toString()
+  console.log('Uploaded:', imageUri)
+
+  const firestoreFileRefThumbnail = ref(storage, `${user.uid}/${dateFolder}/${uploadFileName}-thumb`)
+  await uploadBytes(firestoreFileRefThumbnail, thumbnail, {contentType: 'image/jpeg'})
+  const thumbnailUri = firestoreFileRefThumbnail.toString()
+  console.log('Uploaded thumbnail:', thumbnailUri)
+
+  return { imageUri, thumbnailUri }
+}
+
+interface setOrUpdateImageForLocationInFirestoreProps {
+  userUid: string
+  place_full: string
+  imageData: ImageData
+  place: string
+  country: string
+}
+
+export const setOrUpdateImageForLocationInFirestore = async (data: setOrUpdateImageForLocationInFirestoreProps) => {
+  const { userUid, place_full, place, country } = data
+  const existingDbEntry = await findMediaForLocationDataForUser(userUid, place_full) as MediaData[]
+  if (existingDbEntry.length > 1) {
+    throw new Error('More than 1 DB entry found.')
+  }
+  const docRef = existingDbEntry.length === 0 ?
+    doc(collection(db, `users/${userUid}`, 'media'))
+      : doc(db, `users/${userUid}/media`, existingDbEntry[0].uid)
+  
+  const images: ImageData[] = [
+    ...existingDbEntry[0]?.images ?? [],
+    data.imageData
+  ]
+
+  const dbEntity = {
+    user: userUid,
+    uid: docRef.id,
+    latitude: mean(images.map(image => image.latitude)),
+    longitude: mean(images.map(image => image.longitude)),
+    place,
+    place_full,
+    country,
+    images,
+  }        
+  await setDoc(docRef, dbEntity)
+}
+
+export const getThumbnailAndRotationForFile = async (selectedFile: File) => {
+  const thumbnail = await exifr.thumbnail(selectedFile)
+  const r = await exifr.rotation(selectedFile)
+  
+  if (!thumbnail || !r) {
+    throw new Error('Could not generate thumbnail or get rotation info')
+  }
+  const { deg } = r
+
+  return {
+    thumbnail,
+    rotation: deg
+  }
+}
+
+export const getDateTimeForFile = (selectedFile: File) => {
+  const datetimeFromFilename = getDateFromFilename(selectedFile.name)
+  const datetimeFromFile = DateTime.fromMillis(selectedFile.lastModified).toUTC()
+  const datetime = earliestDateTime(datetimeFromFilename, datetimeFromFile)
+  return datetime
 }
